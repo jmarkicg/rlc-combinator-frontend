6715f8c90a186a11ce39733e777f0d5b7d513202
jmarkic <borovnica17@gmail.com>
2019-04-11 00:15:04 +0200

    Added Pusher on frontend, implemented combinator form screen with logs.

--- src/app/layout/combinator/combinator.component.ts
+++ src/app/layout/combinator/combinator.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ElementEnum} from "../../shared/model/element-enum";
+import {CombinatorModel} from "../../shared/model/combinator-model";
+import {PusherService} from "../../shared/services/pusher.service";
+import {environment} from "../../../environments/environment";
+import {CombinatorService} from "../../shared/services/combinator.service";
 
 @Component({
   selector: 'app-combinator',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombinatorComponent implements OnInit {
 
-  constructor() { }
+  baseElement =  ElementEnum;
+
+  types: string[];
+  combModel: CombinatorModel;
+
+  consoleLog: string = "";
+  combinations: any ;
+
+  submitted: boolean = false;
+  loading: boolean = false;
+  selected: number = 0;
+
+  constructor(private pusherService: PusherService, private combinatorService: CombinatorService) { }
 
   ngOnInit() {
+    this.combModel = new CombinatorModel();
+    this.combModel.allowedErrorPercentage = 5;
+    this.combModel.numGeneratedItems = 20;
+    this.combModel.requestedValue = 50;
+    this.getEnumKeys();
+    this.subScribeToLogs();
   }
+
+  getEnumKeys(){
+    this.types = [];
+    for (var enumMember in this.baseElement) {
+      if(this.combModel.type == null){
+        this.combModel.type = enumMember;
+      }
+      this.types.push(enumMember);
+    }
+  }
+  onSubmit(){
+    this.submitted = true;
+    this.loading = true;
+    this.selected = 0;
+    this.consoleLog ="";
+    this.combinatorService.getCombinations(this.combModel).subscribe((response: any[]) => {
+      console.log(response);
+      this.combinations = response;
+      this.selected = 1;
+      this.loading = false;
+    });
+  }
+
+  subScribeToLogs() {
+    this.pusherService.channel.bind(environment.pusher.event, data => {
+      this.consoleLog += data;
+      this.consoleLog += "<br/>";
+    });
+  }
+
+
 
 }
---
+++ src/app/shared/model/combinator-model.ts
@@ -0,0 +1,8 @@
+import {ElementEnum} from "./element-enum";
+
+export class CombinatorModel {
+  type: string;
+  requestedValue: number;
+  allowedErrorPercentage: number;
+  numGeneratedItems: number;
+}
